b057aca3241954fecaed3267bafc2362a56cf647
Molefi10 <molefik100@gmail.com>
2023-02-20 10:57:00 -0800

    prints single-digit numbers.separated by ,

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ *main - prints all possible combinations of single-digit numbers.
+ *
+ *Description: rints all possible combinations of single-digit numbers.
+ *Numbers must be separated by comma
+ *Return: 0(Success)
+ */
+
+int main(void)
+{
+
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar('0' + i);
+if (i <= 8)
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
+
